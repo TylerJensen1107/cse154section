95dd066dcb355ac7d969a08740b5f17fc68aee94
Tyler Jensen <tylerjensen1107@gmail.com>
2016-02-09 13:09:51 -0800

    lkjsj

--- kitties.php
+++ kitties.php
@@ -2,7 +2,15 @@
 <head>
 	<title>KITTIES</title>
 </head>
-<body>
+	<body>
+		<div>
+			<?php for($i = 1; $i <= 5; $i++) { ?>
+				<img src="kitty1.jpg" />
+
+			<?php } ?>
+
+		</div>	
+
 	
 </body>
 </html>
