7684d9b5f327fc105e37770ff6746f433782912c
Tyler Jensen <tylerjensen1107@gmail.com>
2016-02-16 12:49:47 -0800

    starting section

--- animals.php
+++ animals.php
@@ -3,17 +3,23 @@
 	<title>Animals</title>
 </head>
 <body>
-	<?php  if(isset($_GET["animal"])) {
+	<?php if(isset($_GET["animal"])) {
 			$animal = $_GET["animal"];
 
-	}
 		$files = glob("images/{$animal}*.jpeg");
 
-		foreach ($files as $file) { ?>
-				<p><?= $file ?></p>
+		if(count($files)) { ?>	
+			<p>No files for <?= $animal ?></p>
+
 		<?php }
 
-	?>
+		foreach ($files as $file) { ?>
+				<img src="<?= $file ?>" alt="picture of <?= $animal ?>"></img>
+		<?php }
+	} else {
+	?> <p>Need a parameter</p>
+
+	<?php } ?>
 
 
 </body>
